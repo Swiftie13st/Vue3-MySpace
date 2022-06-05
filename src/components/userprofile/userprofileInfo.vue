e32425871b76b0732ca0ec9f3b561d5922b0b593
Bruce <swiftie13s@163.com>
2022-06-05 17:22:46 +0800

    UserProfileInfo

---
+++ src/components/userprofile/userprofileInfo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="row">
+        <!-- 头像 -->
+        <div class="col-3">
+          <img class="img-fluid"
+            src="https://raw.githubusercontent.com/Swiftie13st/Figurebed/main/img/202206051712008.png"
+            alt=""
+          />
+        </div>
+        <!-- 用户信息 -->
+        <div class="col-9">
+            <div class="username">用户名</div>
+            <div class="fans">粉丝数：123</div>
+            <button type="button" class="btn btn-primary btn-sm">关注</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: "UserProfileInfo",
+
+}
+</script>
+
+<style scoped>
+img {
+    border-radius: 50%;
+}
+
+.username{
+    font-weight: bold;
+}
+
+.fans{
+    font-size: 12px;
+    color: gray;
+}
+
+button {
+    padding: 2px 4px;
+    font-size: 12px;
+}
+
+
+</style>
--- src/views/userprofileView.vue
+++ src/views/userprofileView.vue
@@ -1,16 +1,27 @@
 <template>
-  <ContentBase> 用户动态 </ContentBase>
+  <ContentBase>
+    <div class="row">
+      <div class="col-3">
+       <UserprofileInfo></UserprofileInfo>
+      </div>
+      <div class="col-9">
+        用户动态
+      </div>
+    </div>
+  </ContentBase>
 </template>
 
 <script>
 import ContentBase from "@/components/contentbase.vue";
+import UserprofileInfo from "@/components/userprofile/userprofileInfo.vue";
 // @ is an alias to /src
 
 export default {
   name: "UserProfile",
   components: {
     ContentBase,
-  },
+    UserprofileInfo
+},
 };
 </script>
 <style scoped>
