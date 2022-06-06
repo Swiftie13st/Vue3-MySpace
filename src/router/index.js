96e807015e45642cd413f4b61599f41a8f832c2e
Bruce <swiftie13s@163.com>
2022-06-06 16:17:55 +0800

    bugfix

--- src/router/index.js
+++ src/router/index.js
@@ -8,32 +8,32 @@ import NotFoundView from "../views/notfoundView";
 
 const routes = [
   {
-    path: "/",
+    path: "/myspace/",
     name: "home",
     component: HomeView,
   },
   {
-    path: "/userlist/",
+    path: "/myspace/userlist/",
     name: "userlist",
     component: UserListView,
   },
   {
-    path: "/userprofile/:userId/",
+    path: "/myspace/userprofile/:userId/",
     name: "userprofile",
     component: UserProfileView,
   },
   {
-    path: "/login/",
+    path: "/myspace/login/",
     name: "login",
     component: LoginView,
   },
   {
-    path: "/register/",
+    path: "/myspace/register/",
     name: "register",
     component: RegisterView,
   },
   {
-    path: "/404/",
+    path: "/myspace/404/",
     name: "404",
     component: NotFoundView,
   },
